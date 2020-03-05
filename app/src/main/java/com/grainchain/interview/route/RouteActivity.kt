f123b2e044715749728a3f9fa46de2cea51da8bf
Sergio Adrián Lagunas Pinacho <Shekomaru@Shekomarus-MacBook-Pro.local>
2020-03-05 02:57:10 -0600

    Route info added to map details

--- app/src/main/java/com/grainchain/interview/route/RouteActivity.kt
+++ app/src/main/java/com/grainchain/interview/route/RouteActivity.kt
@@ -1,7 +1,9 @@
 package com.grainchain.interview.route
 
 import android.R
+import android.location.Location
 import android.os.Bundle
+import android.text.format.DateUtils
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -11,13 +13,12 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
-import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolylineOptions
 import com.grainchain.interview.R.id
 import com.grainchain.interview.R.layout
 import com.grainchain.interview.data.Route
-import java.lang.Exception
+import kotlinx.android.synthetic.main.activity_route.info_text
 
 
 class RouteActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -35,13 +36,40 @@ class RouteActivity : AppCompatActivity(), OnMapReadyCallback {
 
         route = intent.extras?.getParcelable("route") ?: Route()
 
-        title = route.name
-
         // add back arrow to toolbar
         if (supportActionBar != null) {
             supportActionBar!!.setDisplayHomeAsUpEnabled(true)
             supportActionBar!!.setDisplayShowHomeEnabled(true)
         }
+
+        showRouteInfo()
+    }
+
+    private fun showRouteInfo() {
+        title = route.name
+
+        val points = route.points
+
+        var distances = floatArrayOf(0f)
+        Location.distanceBetween(
+            points.first().first,
+            points.first().second,
+            points.last().first,
+            points.last().second,
+            distances
+        )
+
+//        val time = DateUtils.formatElapsedTime((route.endTime.time - route.startTime.time) / 1000)
+        //todo: improve time shown
+        val time2 = DateUtils.formatDateRange(
+            this,
+            route.startTime.time,
+            route.endTime.time,
+            (DateUtils.FORMAT_SHOW_TIME or DateUtils.FORMAT_SHOW_WEEKDAY)
+        )
+
+        info_text.text = "Distance: ${distances[0]} km\n" +
+            "Time ellapsed: $time2"
     }
 
     /**
@@ -76,7 +104,7 @@ class RouteActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun showCriticalMarkers(start: Pair<Double, Double>, end: Pair<Double, Double>) {
-        // Add a marker in the starting point and move the camera
+        // Add a marker in the starting point
         val startingPoint = LatLng(start.first, start.second)
         mMap.addMarker(
             MarkerOptions()
@@ -93,8 +121,17 @@ class RouteActivity : AppCompatActivity(), OnMapReadyCallback {
                 .title("End of the route")
         )
 
+        // Move the camera
         try {
-            mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(LatLngBounds(startingPoint, endingPoint), 10))
+            //If we only have one point, or if the start and end points are very close to each other, this will crash
+            mMap.moveCamera(
+                CameraUpdateFactory.newLatLngBounds(
+                    LatLngBounds(
+                        startingPoint,
+                        endingPoint
+                    ), 10
+                )
+            )
         } catch (_: Exception) {
             mMap.moveCamera(CameraUpdateFactory.newLatLng(startingPoint))
         }
